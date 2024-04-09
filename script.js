9ef4034c46552feca923ccc194d95feb60122d91
R-cygr <ellasaerayy@gmail.com>
2024-04-09 17:45:43 +0200

    drawing the bg

---
+++ script.js
@@ -0,0 +1,16 @@
+///BACKGROUND///¨
+
+//CLOUDS//
+//CLOUD 1//
+stroke(255,255,255);
+fill(255,255,255);
+ellipse(130,130,50,30);
+ellipse(110,140,60,30);
+ellipse(150,140,60,30);
+
+//CLOUD 2//
+stroke(255,255,255);
+fill(255,255,255);
+ellipse(330,130,50,30);
+ellipse(310,140,60,30);
+ellipse(350,140,60,30);
